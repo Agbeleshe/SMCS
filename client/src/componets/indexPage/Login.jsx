46b59d60d122db35e2d585e0a6444389cb69f707
daniel <danielagbeleshe@gmail.com>
2023-08-30 02:08:33 +0100

    night-3 commit

--- client/src/componets/indexPage/Login.jsx
+++ client/src/componets/indexPage/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
   const handleLoginSubmit = (e) => {
     e.preventDefault();
     axios
-      .post("http://localhost:3001/login", { email, password })
+      .post("http://localhost:3001/", { email, password })
       .then((result) => {
         setLoginResult(result.data);
         if (result.data === "Success") {
