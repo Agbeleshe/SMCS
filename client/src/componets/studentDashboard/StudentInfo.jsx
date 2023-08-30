46d544a18b80989a09ccecccd4d6aef8e9820c7b
daniel <danielagbeleshe@gmail.com>
2023-08-30 18:08:45 +0100

    newest evening  update

--- client/src/componets/studentDashboard/StudentInfo.jsx
+++ client/src/componets/studentDashboard/StudentInfo.jsx
@@ -1,19 +1,18 @@
-import React, { useEffect, useState } from "react";
-import axios from 'axios';
-import About from "./About";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import HeadProfile from "./HeadProfile";
 import MoreInfo from "./ContactCounselor";
+import About from "./About";
 
 const StudentInfo = () => {
   const [userInfo, setUserInfo] = useState(null);
 
   useEffect(() => {
-    // Get user's email from localStorage or your authentication context
+    // Get user's email from localStorage
     const userEmail = localStorage.getItem("userEmail");
 
     // Fetch user information using the stored email
-    axios
-      .post("http://localhost:3001/fetchUserInfo", { email: userEmail })
+    axios.post("http://localhost:3001/fetchUserInfo", { email: userEmail })
       .then((result) => {
         setUserInfo(result.data);
       })
@@ -23,10 +22,15 @@ const StudentInfo = () => {
   return (
     <div className="bg-base-200 pt-28 md:w-[80%] md:m-auto w-[90%] m-auto h-auto">
       <div className="flex flex-col">
-        {console.log(userInfo)}
-        <HeadProfile userInfo={userInfo} /> {/* Pass user info to HeadProfile */}
-        <MoreInfo userInfo={userInfo} /> {/* Pass user info to MoreInfo */}
-        <About userInfo={userInfo} /> {/* Pass user info to About */}
+        {userInfo ? (
+          <>
+            <HeadProfile userInfo={userInfo} />
+            <MoreInfo userInfo={userInfo} />
+            <About userInfo={userInfo} />
+          </>
+        ) : (
+          <div>Loading...</div>
+        )}
       </div>
     </div>
   );
