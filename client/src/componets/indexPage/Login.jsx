a3ff0ff065a0cf1a17942cf1ffaeb44c587aae6b
daniel <danielagbeleshe@gmail.com>
2023-08-30 06:35:02 +0100

    server-7 working

--- client/src/componets/indexPage/Login.jsx
+++ client/src/componets/indexPage/Login.jsx
@@ -17,11 +17,11 @@ const Login = () => {
   };
 
 
-axios.defaults.withCredentials = true
+axios.defaults.withCredentials = true;
   const handleLoginSubmit = (e) => {
     e.preventDefault();
     axios
-      .post("https://smcsserver.vercel.app/", { email, password })
+      .post("https://smcsclient.vercel.app/", { email, password })
       .then((result) => {
         setLoginResult(result.data);
         if (result.data === "Success") {
