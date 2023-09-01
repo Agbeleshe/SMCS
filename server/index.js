11e0631419dded072f144993e146cd5a7ce753d3
daniel <danielagbeleshe@gmail.com>
2023-09-01 07:06:09 +0100

    trying to fix admin6

--- server/index.js
+++ server/index.js
@@ -23,6 +23,7 @@ app.use(function(req, res, next) {
   next();
 });
 
+
 app.use(express.json());
 
 mongoose.connect(
@@ -61,49 +62,32 @@ app.post("/", (req, res) => {
 
 // Endpoint for user registration
 app.post("/register", (req, res) => {
-  const { email, password } = req.body;
-
-  // Check if user already exists
-  StudentModel.findOne({ email: email })
-    .then((user) => {
-      if (user) {
-        res.json("User already exists");
-      } else {
-        // Create a new user
-        const newUser = new StudentModel({
-          email: email,
-          password: password,
-        });
-
-        newUser
-          .save()
-          .then((user) => {
-            res.json("User registered successfully");
-          })
-          .catch((err) => {
-            console.error("Error while registering user:", err);
-            res.status(500).json(err);
-          });
-      }
+  console.log("Received registration data:", req.body);
+  StudentModel.create(req.body)
+    .then((student) => {
+      console.log("New student created:", student);
+      res.status(201).json(student);
     })
     .catch((err) => {
-      console.error("Error while checking for existing user:", err);
+      console.error("Error creating student:", err);
       res.status(500).json(err);
     });
 });
 
-// Endpoint for uploading result file
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
+// Set up Multer storage configuration for uploading result files
+const storageResultFile = multer.diskStorage({
+  destination: (req, file, cb) => {
     cb(null, "uploads/");
   },
-  filename: function(req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
+  filename: (req, file, cb) => {
+    cb(null, file.originalname); // Use the original filename
   },
 });
 
-const uploadResultFile = multer({ storage: storage });
+// Create a Multer instance for uploading result files
+const uploadResultFile = multer({ storage: storageResultFile });
 
+// Endpoint to upload result files
 app.post("/uploadResult", uploadResultFile.single("resultFile"), async (req, res) => {
   const { email } = req.body;
   const resultFile = req.file;
@@ -113,11 +97,13 @@ app.post("/uploadResult", uploadResultFile.single("resultFile"), async (req, res
     return;
   }
 
-  // Update the user's result file information in the database
+  const resultFilePath = path.join("uploads", resultFile.filename);
+
   try {
+    // Update the user's result file information in the database
     const user = await StudentModel.findOneAndUpdate(
       { email: email },
-      { resultFile: { filename: resultFile.originalname, path: resultFile.path } },
+      { resultFile: { filename: resultFile.originalname, path: resultFilePath } },
       { new: true }
     );
 
@@ -132,5 +118,58 @@ app.post("/uploadResult", uploadResultFile.single("resultFile"), async (req, res
   }
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Endpoint to fetch user information
+app.post("/fetchUserInfo", (req, res) => {
+  const { email } = req.body;
+
+  // Retrieve user information from the database based on the provided email
+  StudentModel.findOne({ email: email })
+    .then((user) => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json("User not found");
+      }
+    })
+    .catch((err) => {
+      console.error("Error while fetching user info:", err);
+      res.status(500).json(err);
+    });
+});
+
+// Endpoint to fetch all students
+app.get("/getAllStudents", (req, res) => {
+  // Fetch all student records from the database
+  StudentModel.find({})
+    .exec()
+    .then((students) => {
+      res.status(200).json(students);
+    })
+    .catch((err) => {
+      console.error("Error fetching students:", err);
+      res
+        .status(500)
+        .json({ error: "An error occurred while fetching students" });
+    });
+});
+
+// Endpoint to download result files
+app.get("/downloadResult/:filename", (req, res) => {
+  const filenameFromDatabase = req.params.filename; // Get the filename from the request
+
+  // Construct the file path to the result file
+  const filePath = path.join(__dirname, "uploads", filenameFromDatabase);
+
+  // Provide the result file for download
+  res.download(filePath, (err) => {
+    if (err) {
+      console.error("Error downloading result file:", err);
+      res.status(500).send("Error downloading result file");
+    }
+  });
+});
+
+// Start the server
+app.listen(3001, () => {
+  console.log("Server running perfectly ...");
+});
