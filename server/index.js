2bd6f4f5b40b36482c67d78bcd35fc5cecb321c0
daniel <danielagbeleshe@gmail.com>
2023-09-01 06:53:46 +0100

    trying to fix admin5

--- server/index.js
+++ server/index.js
@@ -16,6 +16,13 @@ app.use(
   })
 );
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://smcsclient.vercel.app"); // Update this with the URL of your client app
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(express.json());
 
 mongoose.connect(
@@ -54,32 +61,49 @@ app.post("/", (req, res) => {
 
 // Endpoint for user registration
 app.post("/register", (req, res) => {
-  console.log("Received registration data:", req.body);
-  StudentModel.create(req.body)
-    .then((student) => {
-      console.log("New student created:", student);
-      res.status(201).json(student);
+  const { email, password } = req.body;
+
+  // Check if user already exists
+  StudentModel.findOne({ email: email })
+    .then((user) => {
+      if (user) {
+        res.json("User already exists");
+      } else {
+        // Create a new user
+        const newUser = new StudentModel({
+          email: email,
+          password: password,
+        });
+
+        newUser
+          .save()
+          .then((user) => {
+            res.json("User registered successfully");
+          })
+          .catch((err) => {
+            console.error("Error while registering user:", err);
+            res.status(500).json(err);
+          });
+      }
     })
     .catch((err) => {
-      console.error("Error creating student:", err);
+      console.error("Error while checking for existing user:", err);
       res.status(500).json(err);
     });
 });
 
-// Set up Multer storage configuration for uploading result files
-const storageResultFile = multer.diskStorage({
-  destination: (req, file, cb) => {
+// Endpoint for uploading result file
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
     cb(null, "uploads/");
   },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname); // Use the original filename
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
-// Create a Multer instance for uploading result files
-const uploadResultFile = multer({ storage: storageResultFile });
+const uploadResultFile = multer({ storage: storage });
 
-// Endpoint to upload result files
 app.post("/uploadResult", uploadResultFile.single("resultFile"), async (req, res) => {
   const { email } = req.body;
   const resultFile = req.file;
@@ -89,13 +113,11 @@ app.post("/uploadResult", uploadResultFile.single("resultFile"), async (req, res
     return;
   }
 
-  const resultFilePath = path.join("uploads", resultFile.filename);
-
+  // Update the user's result file information in the database
   try {
-    // Update the user's result file information in the database
     const user = await StudentModel.findOneAndUpdate(
       { email: email },
-      { resultFile: { filename: resultFile.originalname, path: resultFilePath } },
+      { resultFile: { filename: resultFile.originalname, path: resultFile.path } },
       { new: true }
     );
 
@@ -110,78 +132,5 @@ app.post("/uploadResult", uploadResultFile.single("resultFile"), async (req, res
   }
 });
 
-  const resultFilePath = path.join("uploads", resultFile.filename);
-
-  // Update the user's result file information in the database
-  StudentModel.findOneAndUpdate(
-    { email: email },
-    { resultFile: { filename: resultFile.originalname, path: resultFilePath } },
-    { new: true }
-  )
-    .then((user) => {
-      if (user) {
-        res.status(200).json(user);
-      } else {
-        res.status(404).json("User not found");
-      }
-    })
-    .catch((err) => {
-      console.error("Error while uploading result file:", err);
-      res.status(500).json(err);
-    });
-
-// Endpoint to fetch user information
-app.post("/fetchUserInfo", (req, res) => {
-  const { email } = req.body;
-
-  // Retrieve user information from the database based on the provided email
-  StudentModel.findOne({ email: email })
-    .then((user) => {
-      if (user) {
-        res.status(200).json(user);
-      } else {
-        res.status(404).json("User not found");
-      }
-    })
-    .catch((err) => {
-      console.error("Error while fetching user info:", err);
-      res.status(500).json(err);
-    });
-});
-
-// Endpoint to fetch all students
-app.get("/getAllStudents", (req, res) => {
-  // Fetch all student records from the database
-  StudentModel.find({})
-    .exec()
-    .then((students) => {
-      res.status(200).json(students);
-    })
-    .catch((err) => {
-      console.error("Error fetching students:", err);
-      res
-        .status(500)
-        .json({ error: "An error occurred while fetching students" });
-    });
-});
-
-// Endpoint to download result files
-app.get("/downloadResult/:filename", (req, res) => {
-  const filenameFromDatabase = req.params.filename; // Get the filename from the request
-
-  // Construct the file path to the result file
-  const filePath = path.join(__dirname, "uploads", filenameFromDatabase);
-
-  // Provide the result file for download
-  res.download(filePath, (err) => {
-    if (err) {
-      console.error("Error downloading result file:", err);
-      res.status(500).send("Error downloading result file");
-    }
-  });
-});
-
-// Start the server
-app.listen(3001, () => {
-  console.log("Server running perfectly ...");
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
